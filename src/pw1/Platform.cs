1a6901ec83b65299931d7b3464f241a9538c9cde
sammaadaniel30 <danielsamac@gmail.com>
2025-06-19 12:12:12 +0200

    Added management for docking and tick updating in the Platform class

--- src/pw1/Platform.cs
+++ src/pw1/Platform.cs
@@ -37,6 +37,38 @@ namespace OOP
                 return $"{id} is ocupied by Train {currentTrain.id}, with {dockingTimeRemaining} ticks to dock";
             }
         }
+
+        public void RequestPlatform(Train train)
+        {
+            if (platformStatus == PlatformStatus.Free)
+            {
+                platformStatus = PlatformStatus.Occupied; // The platform is being used by a train
+                currentTrain = train; // We assign a train to a specific platform
+                dockingTimeRemaining = 2; // We reset the ticks remaning to dock
+                Console.WriteLine($"Train: {train.id} is docking on {id}");
+                train.trainStatus = Train.TrainStatus.Docking; // We change the status of the train to docking
+            }
+            else
+            {
+                Console.WriteLine($"{id} is not free at the moment"); // We tell the user that the platform is not free
+            }
+        }
+
+        public void UpdateTick()
+        {
+            if (platformStatus == PlatformStatus.Occupied && currentTrain != null)
+            {
+                dockingTimeRemaining--; // We substract a tick
+
+                GetStatus();
+
+                if (dockingDuration <= 0)
+                {
+                    currentTrain.trainStatus = Train.TrainStatus.Docked;
+                    Console.WriteLine($"Train {id} has docked in {id}"); 
+                }
+            }
+        }
     }
 
 
